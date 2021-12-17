8b7593156db3afcda075a441960c84573e161e64
Jordan Sullivan <helloandhi.jp@gmail.com>
2021-12-17 14:36:17 -0700

    Complete modelA work

---
+++ models/modelA.js
@@ -0,0 +1,11 @@
+var dinosaur = {
+  name: "Tyrrannosaurus Rex",
+  nickName: "T-Rex",
+  heightInFeet:  17,
+  lengthOfTailInches: 135.5,
+  isTerrrifying: true,
+  isChildsFavorite: true,
+  diet: ["other dinosaurs", "victims in Jurrasic Park"],
+  locationBonesFound: ["Colorado", "Wyoming", "Arizona"]
+};
+console.log(dinosaur.isTerrrifying);
