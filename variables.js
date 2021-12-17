0f4baad89364ea7069efd0f86868b1575141220d
Jordan Sullivan <helloandhi.jp@gmail.com>
2021-12-17 14:18:20 -0700

    Complete variables exercise

---
+++ variables.js
@@ -0,0 +1,16 @@
+var movie = "Breakfast At Tiffany's";
+var isEntertaining = true;
+var movie = "Die Hard";
+console.log(movie);
+
+var num4 = 4;
+var num6 = 6;
+var product = (num4 * num6);
+console.log(product);
+
+var favoriteFoods = ["B.L.T.", "cherries", "sardines"];
+var firstElement = favoriteFoods.shift();
+console.log(favoriteFoods);
+console.log(firstElement);
+
+console.log(movie.length);
