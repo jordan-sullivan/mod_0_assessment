c4306bd23ffffbfd54b65d05d16081de8a472fbe
Jordan Sullivan <helloandhi.jp@gmail.com>
2021-12-17 14:44:54 -0700

    Finish modelB assignment

---
+++ models/modelB.js
@@ -0,0 +1,11 @@
+var developer = {
+  name: "Jordan",
+  program: "Front End",
+  monthsOfExperience: 1,
+  percentageEffort: 100,
+  isExpectedToPass: true,
+  willTryHard: true,
+  resourcesUsed: ["Google", "Page 134", "Class Lessons"],
+  onGoingHoursOfPractice: [ 2, 5, 34, 56, 77],
+};
+console.log(developer.isExpectedToPass);
